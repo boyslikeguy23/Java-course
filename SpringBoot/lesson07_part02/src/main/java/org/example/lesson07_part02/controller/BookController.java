8bbc7b72b7b1e14f38a16c99e99d463fbfc2ee35
boyslikeguy23 <tungxuanmai2003@gmail.com>
2025-03-29 21:54:51 +0700

    fix @Service to @Controller

--- SpringBoot/lesson07_part02/src/main/java/org/example/lesson07_part02/controller/BookController.java
+++ SpringBoot/lesson07_part02/src/main/java/org/example/lesson07_part02/controller/BookController.java
@@ -1,15 +1,14 @@
 package org.example.lesson07_part02.controller;
 
 import org.example.lesson07_part02.entity.Book;
-import org.example.lesson07_part02.repository.BookRepository;
 import org.example.lesson07_part02.service.BookService;
 import org.example.lesson07_part02.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Service
+@Controller
 @RequestMapping("/book")
 public class BookController {
     @Autowired
@@ -35,7 +34,7 @@ public class BookController {
     @GetMapping("/edit/{id}")
     public String editBook(@PathVariable("id") Long id, Model model) {
         model.addAttribute("book", bookService.getBookById(id));
-        model.addAttribute("category", categoryService.getAllCategories());
+        model.addAttribute("categories", categoryService.getAllCategories());
         return "book/book-form";
     }
     @PostMapping("/update/{id}")
